7babc54d1b242ceb2f21124a110f51420152d571
Adam Xiao <iefcuxy@gmail.com>
2021-03-14 13:38:47 +0800

    test pxe-install.sh

--- pxe-install.sh
+++ pxe-install.sh
@@ -1,33 +1,37 @@
 #!/usr/bin/env bash
 
+set -e
+
+# TODO: 定制修改
+# 1. dhcp网段修改
+# 2. tftp镜像配置
+# 3. http安装源配置
+
 # 基于centos7构建(或kylin341?)
-# dhcp + tftp服务 + 一个测试的http
+# dhcp + tftp + http
 
 # keyword "pxe install centos"
 # refer https://www.tecmint.com/install-pxe-network-boot-server-in-centos-7/
 
-yum install -y dnsmasq dhcpd syslinux tftp-server
-systemctl enable dnsmasq tftp
+yum install -y dnsmasq syslinux httpd createrepo
+systemctl enable dnsmasq httpd
 tftp_dir=/var/lib/tftpboot
 
-# dhcp 配置 TODO: 需要自己修改
+mkdir -p $tftp_dir
+chown nobody:nobody $tftp_dir
+
 cat > /etc/dnsmasq.d/pxe-dhcp.conf << EOF
-interface=eth0
+interface=eth1
 bind-interfaces
-domain=template-centos
 
-dhcp-range=eth0,192.168.56.230,192.168.56.253,255.255.255.0,1h
+dhcp-range=192.168.56.230,192.168.56.253,255.255.255.0,1h
 dhcp-option=3,192.168.56.1
-dhcp-option=6,192.168.56.1
 dhcp-option=6,8.8.8.8
-server=8.8.4.4
-dhcp-option=28,10.0.0.255
-dhcp-option=42,0.0.0.0
 
-dhcp-boot=pxelinux/pxelinux.0,pxeserver,192.168.56.1
+pxe-service=x86PC, "network installer", pxelinux
 
-pxe-prompt="Press F8 for menu.", 2
-pxe-service=x86PC, "Install CentOS from network server", pxelinux
+enable-tftp
+tftp-root=/var/lib/tftpboot
 EOF
 
 # 构建tftp目录结构
@@ -50,11 +54,3 @@ label -ksvd-818
   kernel image/ksvd818/vmlinuz
   append initrd=image/ksvd818/initrd.img method=http://192.168.56.2/ksvd818 ks=http://192.168.56.2/ksvd818/kickstart/ks.cfg devfs=nomount inst.vnc inst.vncpassword=ksvd2020
 EOF
-
-# TODO: 作为nat网关配置
-# iptables -t nat -I POSTROUTING -o Mdvs -j MASQUERADE
-# sysctl -w net.ipv4.ip_forward=1
-
-# TODO: /var/www/html
-# 构建软件仓库源
-# createrepo -d -g comps.xml .
