195d6e601ba421df8c822a5e130b18c3c8d51710
Adam Xiao <iefcuxy@gmail.com>
2021-03-28 19:18:30 +0800

    dnsmasq support bios and efi, and arm efi

--- install-dnsmasq.sh
+++ install-dnsmasq.sh
@@ -17,7 +17,16 @@ dhcp-range=192.168.56.100,192.168.56.200,255.255.255.0,1h
 dhcp-option=3,192.168.56.1
 dhcp-option=6,114.114.114.114
 
-dhcp-boot=pxelinux/pxelinux.0,pxeserver,192.168.56.2
+dhcp-match=set:efi-x86_64,option:client-arch,7
+dhcp-match=set:efi-x86_64,option:client-arch,9
+dhcp-match=set:efi-x86,option:client-arch,6
+dhcp-match=set:efi-arm,option:client-arch,11
+dhcp-match=set:bios,option:client-arch,0
+dhcp-boot=tag:efi-x86_64,shim.efi,,10.20.1.100
+dhcp-boot=tag:efi-x86,shim.efi,,10.20.1.100
+dhcp-boot=tag:efi-arm,arm/shim.efi,,10.20.1.100
+dhcp-boot=tag:bios,pxelinux/pxelinux.0,,10.20.1.100
+
 EOF
 
 # 检查dhcp服务是否正常起来
